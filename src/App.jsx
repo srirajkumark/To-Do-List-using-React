5f69bb859718f8ee8bea66d762e1db1972316208
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-11-02 23:55:02 +0530

    JSX Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,33 @@ import "./styles.css"
 
 
 export default function App() {
-  return "Hi"
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+              Item 1
+          </label>
+          <button className="btn btn-danger">Danger</button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+              Item 2
+          </label>
+          <button className="btn btn-danger">Danger</button>
+        </li>
+      </ul>
+    </>
+  )
 }
